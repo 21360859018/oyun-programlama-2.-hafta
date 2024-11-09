e281dff3bc2f8aff46fc5f0cf7b6868e16b9fc59
İrem Aytaş <113477042+21360859018@users.noreply.github.com>
2024-11-09 18:58:47 +0300

    lazer_sc.cs

--- lazer_sc.cs
+++ lazer_sc.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Laser_sc : MonoBehaviour
 {
-    float speed = 1.0f;
 
-    // Start is called before the first frame update
+
+[SerializeField]
+private float _speed=8.0f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0,1,0) * Time.deltaTime * speed);
+       transform.Translate(Vector3.up*Time.deltaTime*_speed);
 
-        if(transform.position.y > 7.0f){
-            Destroy(this.gameObject);
-        }
-    }
+       if(transform.position.y>7f)
+       { 
+        if(transform.parent != null)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+        Destroy(this.gameObject);
+       }
+
+    }
+
+
+
+
 }
